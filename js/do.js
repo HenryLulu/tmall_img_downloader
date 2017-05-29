bfcc352b20fa69b1667d6f108ae224f9ee563113
HenryLulu <why318why@gmail.com>
2017-05-29 09:45:26 +0800

    support taobao

--- js/do.js
+++ js/do.js
@@ -1,10 +1,12 @@
 $(".tb-thumb img").each(function(){
-    $(this).click(function(){
-        var img = $(this).attr("src").replace('60x60','430x430')
+    $(this).click(function(e){
+        e.stopPropagation();
+        var img = $(this).attr("src").replace('60x60','430x430').replace('50x50.jpg_.webp','430x430.jpg')
         var alink = document.createElement("a");
         alink.href = img;
         alink.download = "testImg.jpg";
         alink.click();
     })
 })
+$("#J_UlThumb").css('background','#eeeeee')
 
